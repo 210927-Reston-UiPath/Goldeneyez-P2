f0b9d85046653765844fe69eff357918c020485c
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2021-11-08 14:53:42 -0800

    user interface and job searching

---
+++ database.sql
@@ -0,0 +1,8 @@
+select * from jobs;
+select * from userlogin;
+select * from users;
+
+insert into users (username, userlocation,userjob,useremail)
+values ('Aaron','Davis','Teacher','alao@email.com')
+
+delete from users where userid = '5'
